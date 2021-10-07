1cdaf9aa92ad3f8fb2f002d0275eb8ae3e005516
sin1102 <titanic1102341@gmail.com>
2021-10-07 20:36:19 +0700

    load library

--- app/src/main/java/com/example/epub/Login.java
+++ app/src/main/java/com/example/epub/Login.java
@@ -106,7 +106,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
                     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
                     if(user.isEmailVerified()){
-                        startActivity(new Intent(Login.this, Display1.class));
+                        startActivity(new Intent(Login.this, SideBar.class));
                     }
                     else
                     {
