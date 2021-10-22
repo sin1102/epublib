61961359b1e25ff5b787a6ccac45ef4c57f40a8e
starluckymc201 <anhtrungdinh210@gmail.com>
2021-10-22 18:59:14 +0700

    Display Picture on SideBar

--- app/src/main/java/com/example/epub/Display/SideBar.java
+++ app/src/main/java/com/example/epub/Display/SideBar.java
@@ -1,6 +1,7 @@
 package com.example.epub.Display;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,14 +10,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.epub.User.ChangePassword;
 import com.example.epub.User.Login;
 import com.example.epub.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class SideBar extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
 
@@ -29,17 +43,18 @@ public class SideBar extends AppCompatActivity implements  NavigationView.OnNavi
     public DrawerLayout mDrawerLayout;
     public NavigationView navigationView;
 
-//     ImageView imageUser;
-//     EditText txtUsername;
-//     EditText txtEmail;
-//
-//    StorageReference storageReference;
-//
-//    FirebaseAuth fAuth;
-//    FirebaseFirestore fStore;
-//    FirebaseUser fUser;
-//
-//    String userID;
+
+    ImageView imageUser;
+    TextView txtFullname;
+    TextView txtEmail;
+
+    StorageReference storageReference;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    FirebaseUser fUser;
+
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,31 +74,33 @@ public class SideBar extends AppCompatActivity implements  NavigationView.OnNavi
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-//        imageUser =  (ImageView) mDrawerLayout.findViewById(R.id.imageUser);
-//        txtUsername = navigationView.findViewById(R.id.txtUsername);
+        imageUser = (ImageView) findViewById(R.id.imageUser);
+        txtFullname = (TextView) findViewById(R.id.txtFullname);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
 
 
-//        fAuth = FirebaseAuth.getInstance();
-//        fStore = FirebaseFirestore.getInstance();
-//        storageReference = FirebaseStorage.getInstance().getReference();
-//        fUser = fAuth.getCurrentUser();
-//        userID = fAuth.getCurrentUser().getUid();
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        fUser = fAuth.getCurrentUser();
+        userID = fAuth.getCurrentUser().getUid();
 
-//        StorageReference profileRef = storageReference.child("Users/" + fAuth.getCurrentUser().getUid()+ "/profile.jpg");
-//        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//            @Override
-//            public void onSuccess(Uri uri) {
-//                Picasso.get().load(uri).into(imageUser);
-//            }
-//        });
+        StorageReference profileRef = storageReference.child("Users/" + fAuth.getCurrentUser().getUid()+ "/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(imageUser);
+            }
+        });
 
-//        DocumentReference documentReference = fStore.collection("Users").document(userID);
-//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-//                txtUsername.setText(value.getString("fullname"));
-//            }
-//        });
+        DocumentReference documentReference = fStore.collection("Users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                txtFullname.setText(value.getString("fullname"));
+                txtEmail.setText(value.getString("email"));
+            }
+        });
 
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -165,8 +182,6 @@ public class SideBar extends AppCompatActivity implements  NavigationView.OnNavi
                 editor.putString("remember", "false");
                 editor.apply();
 
-                startActivity(new Intent(this, Login.class));
-                FirebaseAuth.getInstance().signOut();
                 finish();
 
             default:
